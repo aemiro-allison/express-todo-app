a75eaf88d2cbbc1b5ec500c45b18a12d16fcde98
Aemiro Allison <aemiro.allison@outlook.com>
2017-08-27 16:45:22 -0400

    add and test index.ejs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const logger = require('morgan');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -17,7 +17,9 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('test working');
+  res.render('index', {
+    page: 'index',
+  });
 });
 
 app.get('*', (req, res) => {
