b776f9ba5594f0f30389ceedf8005d1cb5e04ae2
Aemiro Allison <aemiro.allison@outlook.com>
2017-08-27 22:05:21 -0400

    finish production ver

--- db/seeds/todos.sql
+++ db/seeds/todos.sql
@@ -1,3 +1,8 @@
 INSERT INTO todos (title, completion, category, description)
 VALUES
-('EXAMPLE TODO', 'to-do', 'DEMO', 'Create todos just like this one');
+('EXAMPLE TODO', 'done', 'DEMO', 'Create todos just like this one'),
+
+('Walk the dog', 'to-do', 'CHORE', 'Remember to walk the dog or else :('),
+('Walk the latest Transformers', 'to-do', 'FUN', 'Because maybe its cool? Im not sure.'),
+('Do homework', 'done', 'WORK', 'It must be done.'),
+('Get good ZZZzzz', 'to-do', 'FUN', 'Sleep is fun but did you know that sleep could also be considered as work.');
